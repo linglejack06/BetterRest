dc19df14566a95eec76058c4a453aa4eddc2d43e
Jack Lingle <captainjack0818@gmail.com>
2023-11-30 17:56:54 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jack Lingle on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
